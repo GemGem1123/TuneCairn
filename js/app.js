213dcf1c81d6ff6395e98e73a5d2edcaf502da9c
Gemma Watson <gemmamwatson@gmail.com>
2026-06-22 18:51:57 +0100

    working tune from

--- js/app.js
+++ js/app.js
@@ -1,34 +1,52 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // render the tune when the page loads
+  renderTuneList();
+
+  const overlay = document.getElementById('overlay');
+
+  //OPEN
+  document.querySelector('.add-btn').addEventListener('click', () => {
+    overlay.classList.add('overlay--visible');
+  });
+
+  //CANCEL
+  document.getElementById('form-cancel').addEventListener('click', () => {
+    overlay.classList.remove('overlay--visible');
+  });
+
+  //SAVE
+  document.getElementById('form-save').addEventListener('click', () => {
+
+    const name = document.getElementById('tune-name').value.trim();
+    const type = document.getElementById('tune-type').value;
+    const key = document.getElementById('tune-key').value.trim();
+    const notes = document.getElementById('tune-notes').value.trim();
+
+    //Validation
+    if (!name) {
+      alert('Yer tune needs a name!')
+      return
+    }
+
+    const tune = {
+      id: String(Date.now()),
+      name: name,
+      type: type.toLowerCase(),
+      key: key,
+      notes: notes,
+      sets: [],
+      sessionId: null,
+      lastPractised: new Date().toISOString()
+    }
+
+    saveTune(tune);
     renderTuneList();
 
-    // add a tune 
-    //shows a wee prompt for just now 
-    document.querySelector('.add-btn').addEventListener('click', () => {
-
-        const name = prompt('Tune name?');
-        if (!name) return;
-
-        const type = prompt('Type? (reel, jig, waltz, hornpipe...)');
-        if (!type) return;
-
-        const key = prompt('Key? (e.g. D major, E dorian...)');
-        if (!key) return;
-
-        const tune = {
-        id: String(Date.now()),
-        name: name,
-        type: type.toLowerCase(),
-        key: key,
-        notes: '',
-        sets: [],
-        sessionId: null,
-        lastPractised: new Date().toISOString()
-        };
-
-        saveTune(tune);
-        renderTuneList();
-    });
+    //Clean stuff up
+    overlay.classList.remove('overlay--visible');
+    document.getElementById('tune-name').value = '';
+    document.getElementById('tune-key').value = '';
+    document.getElementById('tune-notes').value = '';
+  });
 });
